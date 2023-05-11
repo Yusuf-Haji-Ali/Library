39422f3fb27cd105dd9d75b20dcbd688b12f9a89
Yusuf <yhajiali7@gmail.com>
2023-05-11 18:28:06 +0100

    Cart books, quantity and total update feature

--- src/App.js
+++ src/App.js
@@ -6,14 +6,70 @@ import Footer from "./components/Footer";
 import Books from "./pages/Books";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import React, { useEffect, useState } from "react";
+
+// const dupeItem = cart.find((item) => +item.id === book.id);
+//     if (dupeItem) {
+//       setCart(
+//         cart.map((item) => {
+//           if (item.id === dupeItem.id) {
+//             return {
+//               ...item,
+//               quantity: item.quantity + 1,
+//             };
+//           } else {
+//             return item;
+//           }
+//         })
+//       );
+//     } else {
+//       setCart([...cart, { ...book, quantity: 1 }]);
+//     }
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function removeFromCart(book) {}
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
   return (
     <Router>
-      <Nav />
+      <Nav cart={cart} />
       <Route path="/" exact component={Home} />
       <Route path="/books" exact render={() => <Books books={books} />} />
-      <Route path="/books/:id" render={() => <BookInfo books={books} />} />
+      <Route
+        path="/books/:id"
+        render={() => (
+          <BookInfo books={books} addToCart={addToCart} cart={cart} />
+        )}
+      />
+      <Route
+        path="/cart"
+        render={() => (
+          <Cart books={books} cart={cart} changeQuantity={changeQuantity} />
+        )}
+      />
       <Footer />
     </Router>
   );
